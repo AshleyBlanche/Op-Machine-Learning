5a7e89a1d1da078cb530052a63a9edc8fb48b6e4
AshleyBlanche <76812539+AshleyBlanche@users.noreply.github.com>
2021-05-05 15:50:07 +0000

    Update logs.py

--- Exercise_starter_files/logs.py
+++ Exercise_starter_files/logs.py
@@ -5,7 +5,7 @@ from azureml.core.webservice import Webservice
 ws = Workspace.from_config()
 
 # Set with the deployment name
-name = "bank-53"
+name = "run-54"
 
 # load existing web service
 service = Webservice(name=name, workspace=ws)
